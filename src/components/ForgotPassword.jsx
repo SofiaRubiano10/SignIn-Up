7efa78ed655f003fa3b20026ffc3db2301dcd3a5
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-06-19 21:14:57 +0200

    Utilizando Formik

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,8 +1,15 @@
 import { useContext } from "react";
+import { useFormik } from "formik";
 import SignContext from "../context/SignContext";
 
 const ForgotPassword = () => {
   const { setStep } = useContext(SignContext);
+  // Hook useFormik que permite manejar la info del formulario
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+  });
   return (
     <main>
       <h3>Forgot Password?</h3>
@@ -10,7 +17,14 @@ const ForgotPassword = () => {
         <form autoComplete="off">
           <fieldset>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" autoFocus />
+            <input
+              type="email"
+              id="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              autoFocus
+            />
             <p className="error">Valid email required</p>
           </fieldset>
           <button type="submit">Remember me</button>
